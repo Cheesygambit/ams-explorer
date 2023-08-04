612a1d12bd5119c02fde90fd5556ddc23ffba874
Kevin Jung <44682519+Cheesygambit@users.noreply.github.com>
2023-08-04 21:27:19 +0200

    Responsive

--- src/routes/tracks/+page.svelte
+++ src/routes/tracks/+page.svelte
@@ -4,15 +4,15 @@
 </script>
 
 <div class="container h-full mx-auto">
-    <div class="grid grid-cols-4 gap-4 pt-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 p-4">
         {#each tracks as track}
         <a class="card bg-initial card-hover overflow-hidden" href="/">
-            <header>
+            <header class="relative inline-block">
                 <img class="w-full" src={track.thumbnail} alt="Track thumbnail">
+                <span class="badge variant-filled-primary absolute z-10 top-3 left-3">{track.dlc}</span>
             </header>
-            <div class="p-4 space-y-2">
+            <div class="p-2 text-center">
                 <h3 class="h3">{track.title}</h3>
-                <span class="badge variant-filled-primary">{track.dlc}</span>
             </div>
         </a>
         {/each}
